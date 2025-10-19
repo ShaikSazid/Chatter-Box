4f57ceb31d398c4b5c9a194e7c6f85fdc5da8fbf
Shaik Sazid <shaiksazid7386@gmail.com>
2025-10-20 01:26:17 +0530

    Fixed scroll bug in ChatWindow

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -35,7 +35,7 @@ const ChatWindow: React.FC = () => {
     }
 
     return (
-        <div className="flex-1 flex flex-col bg-gray-800/30">
+        <div className="flex-1 flex flex-col bg-gray-800/30 min-h-0">
             <div className="flex-1 overflow-y-auto">
                 <div className="max-w-4xl mx-auto p-4 md:p-6 space-y-4">
                     {messages.map((msg) => (
@@ -52,9 +52,9 @@ const ChatWindow: React.FC = () => {
 
 const DashboardPage: React.FC = () => {
   return (
-    <div className="flex h-screen w-full">
+    <div className="flex h-screen w-full overflow-hidden">
       <Sidebar />
-      <main className="flex-1 flex flex-col">
+      <main className="flex-1 flex flex-col min-h-0">
         <ChatWindow />
       </main>
     </div>
